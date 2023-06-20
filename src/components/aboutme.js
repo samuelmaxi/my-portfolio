f4539e98e2bfe796869f94ef7238aa2956abaeab
samuelmaxi <samuelgaraujo87@gmail.com>
2023-06-20 10:34:17 -0300

    add css

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,14 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
 import { React } from 'react'
 
-export function AboutMe(){
-  return(
+export function AboutMe() {
+  return (
     <section className="about-me">
-        <div className="text">
-          <h1 className='title'>Hi, i'm Samuel Máximo</h1>
-          <p className='contet'>Sou estudante de Sistemas de informação na Uninassau - THE</p>
-        </div>
-        <div className="image"></div>
-      </section>
+      <div className="text">
+        <h1 className='title'>Olá, eu sou Samuel Máximo</h1>
+        <p className='contet'>
+        Sou um estudante de programação apaixonado por tecnologia e busca constante por conhecimento. Tenho habilidades em desenvolvimento web, mobile e lógica de programação. Sou comprometido com minhas tarefas, criativo, dedicado, aprendizagem rápida, capaz de resolver problemas complexos e trabalhar eficientemente tanto de forma independente quanto em equipe. Este portfólio representa minha jornada de aprendizado, demonstrando minha capacidade de resolver desafios e desenvolver projetos inovadores. Fique à vontade para explorar meu portfólio e entrar em contato para discutir oportunidades de colaboração!!!
+        </p>
+      </div>
+      <div className="image"></div>
+    </section>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import '../styles/header.scss'
 
 export function Header(){
   return(
